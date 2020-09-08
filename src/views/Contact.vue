d885d0db44c886eb42b5d42bc7fa37cc48b08da5
Achilles45 <achillesusuoyibo7@gmail.com>
2020-09-07 23:02:11 -0700

    Changed phone number

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -17,7 +17,7 @@
           <h6><i class="fa fa-envelope"></i>&nbsp; Support Mail</h6>
            <p><a href="mailto:support@globaldeliveryservice.net" class="">support@globaldeliveryservice.net</a></p>
              <h6><i class="fa fa-phone"></i>&nbsp; Phone Number</h6>
-           <p><a href="tel:+1 (347) 899‑3855" class="">+1 (347) 899‑3855</a></p><br>
+           <p><a href="tel:+1 (213) 986‑8797" class="">+1 (213) 986‑8797</a></p><br>
         </div>
         <div class="col-md-5">
           <form @submit.prevent="sendMessage()">
